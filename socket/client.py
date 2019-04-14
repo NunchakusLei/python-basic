862171362202a209dbe887eae137333b189a1909
Chenrui Lei <chenrui@ualberta.ca>
2019-04-13 21:32:29 -0600

    Redesign client so it can be inheritent easier.

--- socket/client.py
+++ socket/client.py
@@ -1,8 +1,9 @@
-import socket
 import argparse
+import logging
+import socket
 import threading
-import time
 from lib import load_host_config, config_logger
+
 
 def config_argparser(ap):
     ap.add_argument(
@@ -13,62 +14,99 @@ def config_argparser(ap):
     )
 
 
-class SocketClient:
-    def __init__(self, host_config):
-        addr = (host_config.hostname, host_config.port)
-        self.config = host_config
+class SimpleSocketClient:
+    def __init__(self, host_config, log_level=logging.INFO):
+        # Connect to server
         self.s = socket.socket()
-        self.s.connect(addr)
-        self.s.settimeout(1.0)
-        self.active = True
-        log.info("Connected to server.")
+        self.s.connect((host_config.hostname, host_config.port))
+        self.s.settimeout(0.5)
 
-        self.sent_msg_count = 0
+        # Client variables
+        self.log = config_logger(self.__class__.__name__, log_level=log_level)
+        self._config = host_config
+        self.running = False
 
-        self.recv_thread = threading.Thread(
-            target=self.incomming_handler, args=(self.s, addr)
-        )
+    def run(self):
+        # Initialize connection
+        if not self.init_connection():
+           self.log.critical('Failed to initialize connection.')
+           self.stop()
+           return
+
+        self.running = True
+        self.recv_thread = threading.Thread(target=self._await_svr_msg)
         self.recv_thread.start()
 
-    def send(self, msg):
-        data = input("Type in the message to send > ")
-        self.sent_msg_count += 1
+        self._await_usr_cmd()
 
-        # TCP
-        # log.info("Sending message: {:s}".format(data))
-        # s.send(data.encode())
-        msg = "{:} {:}".format(data, self.sent_msg_count)
-        log.info("Sending message: {:s}".format(msg))
-        self.s.send(msg.encode(host_config.codec))
+    def stop(self):
+        self.running = False
 
-        # # UDP
-        # for i in range(10):
-            # msg = "{:} {:}".format(data, i)
-            # log.info("Sending message: {:s}".format(msg))
-            # s.sendto(msg.encode(), addr)
-            # time.sleep(1)
+        # Client exit operation
+        # TODO
 
-    def incomming_handler(self, conn, addr):
-        while self.active:
+        self.log.info('Client exit.')
+
+    def _await_usr_cmd(self):
+        while self.running:
+            # Get user command
+            cmd = self.get_usr_cmd()
+
+            # Open new thread to handle user command
+            new_thread = threading.Thread(target=self.cmd_callback, args=(cmd,))
+            new_thread.start()
+            # self._threads.append(new_thread)
+
+    def _await_svr_msg(self):
+        addr = self.s.getpeername()
+        while self.running:
             try:
-                data = conn.recv(1024)
+                data = self.s.recv(1024)
             except socket.timeout:
                 continue
 
             if len(data) == 0:
-                log.info("{:}:{:} hang up.".format(addr[0], addr[1]))
+                self.log.info("{:}:{:} hang up.".format(addr[0], addr[1]))
                 exit()
 
-            log.debug('{:}:{:} say: {:}'.format(addr[0], addr[1], data.decode(self.config.codec)))
-            msg = data.decode(self.config.codec)
+            msg = data.decode(self._config.codec)
+            # self.log.debug('{:}:{:} say: {:}'.format(addr[0], addr[1], msg))
 
-    def hangup(self):
-        self.active = False
-        self.recv_thread.join()
-        self.s.close()
+            # Open new thread to handle server message
+            new_thread = threading.Thread(target=self.msg_callback, args=(msg,))
+            new_thread.start()
+            # self._threads.append(new_thread)
 
-    def message_handler(self, msg):
-        return None
+    def init_connection(self):
+        self.log.warning('Not validation when connecting initialization (default).')
+        return True
+
+    def get_usr_cmd(self):
+        cmd = input("Type in command > ")
+        return cmd
+
+    def msg_callback(self, msg):
+        self.log.info('Got message: {:}'.format(msg))
+        return msg
+
+    def cmd_callback(self, cmd):
+        if cmd.upper() == 'EXIT':
+            self.stop()
+            return
+        self.send(cmd)
+
+    def send(self, msg):
+        conn = self.s
+        addr = self.s.getpeername()
+        try:
+            data = msg.encode(self._config.codec)
+            # TCP
+            conn.send(data)
+            # # UDP
+            # conn.sendto(data, addr)
+            self.log.debug('Sending message: {:}'.format(msg))
+        except BrokenPipeError:
+            self.log.error('Failed to send message: {:}'.format(msg))
 
 
 if __name__ == '__main__':
@@ -78,14 +116,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Load configurations
-    app_name = __file__.split('/')[-1].split('.')[0]
-    log = config_logger(app_name)
     host_config = load_host_config(args.host_config)
 
     # Setup socket
-    my_client = SocketClient(host_config)
-    for i in range(5):
-        my_client.send("message")
-    my_client.hangup()
-
+    my_client = SimpleSocketClient(host_config)
+    my_client.run()
 
