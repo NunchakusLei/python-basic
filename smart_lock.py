7bbdcaf37574597e65952472a969a47ce6513ba8
Chenrui Lei <chenrui@ualberta.ca>
2020-05-10 01:59:54 -0600

    Added smart_lock example

---
+++ smart_lock.py
@@ -0,0 +1,7 @@
+password = input("Enter the passwrod to open the door > ")
+correct_password = "123456"
+
+if password == correct_password:
+    print("The password is correct. Openning the door.")
+else:
+    print("The password is incorrect. No open the door.")
